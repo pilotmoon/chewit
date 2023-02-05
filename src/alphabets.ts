3c0fe0b75884c6cd8daa22ebe86de6270a3cb130
Nick Moore <nick@pilotmoon.com>
2023-02-05 22:44:15 +0000

    alphabet comments

--- src/alphabets.ts
+++ src/alphabets.ts
@@ -1,8 +1,8 @@
 // some common base-n encoding alphabets
 export const alphabets = {
   base62: "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz",
-  base58Flickr: "123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ",
-  base58Satoshi: "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz",
+  base58Flickr: "123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ", // missing 0, l, I, O
+  base58Satoshi: "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz", // missing 0, I, O, l
   base32Crockford: "0123456789ABCDEFGHJKMNPQRSTVWXYZ", // missing I, L, O, U
   base16: "0123456789ABCDEF",
   base10: "0123456789",
