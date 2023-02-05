87d458cdb1d3b34858a32388fcce2e76b4d2ed0f
Nick Moore <nick@pilotmoon.com>
2023-02-05 21:21:35 +0000

    Add comment on Crockford alphabet

--- alphabets.ts
+++ alphabets.ts
@@ -3,7 +3,7 @@ export const alphabets = {
   base62: "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz",
   base58Flickr: "123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ",
   base58Satoshi: "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz",
-  base32Crockford: "0123456789ABCDEFGHJKMNPQRSTVWXYZ",
+  base32Crockford: "0123456789ABCDEFGHJKMNPQRSTVWXYZ", // missing I, L, O, U
   base16: "0123456789ABCDEF",
   base10: "0123456789",
 };
