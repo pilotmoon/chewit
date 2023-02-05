c2188a3592f757f567adba944666fd3ed746e9b6
Nick Moore <nick@pilotmoon.com>
2023-02-05 22:12:56 +0000

    Add ava tests

--- test.js
+++ test.js
@@ -1,25 +1,49 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const index_1 = require("./index");
-console.log((0, index_1.baseConvert)([1, 0, 0], 10, 16));
-console.log((0, index_1.baseEncode)([0xff, 0xff], index_1.alphabets.base10));
-console.log((0, index_1.randomString)());
-console.log((0, index_1.randomString)({ length: 32 }));
-console.log(
-  (0, index_1.randomString)({
+const ava_1 = require("ava");
+(0, ava_1.default)("baseConvert", (t) => {
+  t.deepEqual((0, index_1.baseConvert)([1, 0, 0], 10, 16), [0, 6, 4]);
+});
+(0, ava_1.default)("baseEncode", (t) => {
+  t.is(
+    (0, index_1.baseEncode)([0xff, 0xff], index_1.alphabets.base10),
+    "65535",
+  );
+});
+(0, ava_1.default)("randomString with no arguments", (t) => {
+  const r0 = (0, index_1.randomString)();
+  t.assert(r0.length == 24);
+});
+(0, ava_1.default)("randomString with length", (t) => {
+  const r0 = (0, index_1.randomString)({ length: 32 });
+  t.assert(r0.length == 32);
+});
+(0, ava_1.default)("randomString base58Flickr", (t) => {
+  const r0 = (0, index_1.randomString)({
     length: 5,
     alphabet: index_1.alphabets.base58Flickr,
-  }),
-);
-console.log(
-  (0, index_1.randomString)({
-    length: 6,
+  });
+  t.assert(r0.length == 5);
+});
+(0, ava_1.default)("randomString base32Crockford", (t) => {
+  const r0 = (0, index_1.randomString)({
+    length: 5,
     alphabet: index_1.alphabets.base32Crockford,
-  }),
-);
-console.log(
-  (0, index_1.randomString)({ length: 6, alphabet: index_1.alphabets.base16 }),
-);
-console.log(
-  (0, index_1.randomString)({ length: 3, alphabet: index_1.alphabets.base10 }),
-);
+  });
+  t.assert(r0.length == 5);
+});
+(0, ava_1.default)("randomString base16", (t) => {
+  const r0 = (0, index_1.randomString)({
+    length: 6,
+    alphabet: index_1.alphabets.base16,
+  });
+  t.assert(r0.length == 6);
+});
+(0, ava_1.default)("randomString base10", (t) => {
+  const r0 = (0, index_1.randomString)({
+    length: 3,
+    alphabet: index_1.alphabets.base10,
+  });
+  t.assert(r0.length == 3);
+});
--- test.ts
+++ test.ts
@@ -1,25 +1,41 @@
 import { alphabets as a, baseConvert, baseEncode, randomString } from "./index";
-console.log(
-  baseConvert([1, 0, 0], 10, 16),
-);
-console.log(
-  baseEncode([0xff, 0xff], a.base10),
-);
-console.log(
-  randomString(),
-);
-console.log(
-  randomString({ length: 32 }),
-);
-console.log(
-  randomString({ length: 5, alphabet: a.base58Flickr }),
-);
-console.log(
-  randomString({ length: 6, alphabet: a.base32Crockford }),
-);
-console.log(
-  randomString({ length: 6, alphabet: a.base16 }),
-);
-console.log(
-  randomString({ length: 3, alphabet: a.base10 }),
-);
+
+import test from "ava";
+
+test("baseConvert", (t) => {
+  t.deepEqual(baseConvert([1, 0, 0], 10, 16), [0, 6, 4]);
+});
+
+test("baseEncode", (t) => {
+  t.is(baseEncode([0xff, 0xff], a.base10), "65535");
+});
+
+test("randomString with no arguments", (t) => {
+  const r0 = randomString();
+  t.assert(r0.length == 24);
+});
+
+test("randomString with length", (t) => {
+  const r0 = randomString({ length: 32 });
+  t.assert(r0.length == 32);
+});
+
+test("randomString base58Flickr", (t) => {
+  const r0 = randomString({ length: 5, alphabet: a.base58Flickr });
+  t.assert(r0.length == 5);
+});
+
+test("randomString base32Crockford", (t) => {
+  const r0 = randomString({ length: 5, alphabet: a.base32Crockford });
+  t.assert(r0.length == 5);
+});
+
+test("randomString base16", (t) => {
+  const r0 = randomString({ length: 6, alphabet: a.base16 });
+  t.assert(r0.length == 6);
+});
+
+test("randomString base10", (t) => {
+  const r0 = randomString({ length: 3, alphabet: a.base10 });
+  t.assert(r0.length == 3);
+});
