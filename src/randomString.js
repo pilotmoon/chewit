b6ff6e14399ba91bea9e85575a48da72763e014a
Nick Moore <nick@pilotmoon.com>
2023-02-06 18:33:14 +0000

    Allow custom RNG for randomstring

--- src/randomString.js
+++ src/randomString.js
@@ -4,18 +4,20 @@ exports.randomString = void 0;
 const node_crypto_1 = require("node:crypto");
 const alphabets_1 = require("./alphabets");
 const baseEncode_1 = require("./baseEncode");
+// default RNG, for node
+function nodeRng(size) {
+  return [...(0, node_crypto_1.randomBytes)(size)];
+}
 // generate a random string of the given length using the given alphabet
 function randomString(
-  { length = 24, alphabet = alphabets_1.alphabets.base62 } = {},
+  { length = 24, alphabet = alphabets_1.alphabets.base62, rng = nodeRng } = {},
 ) {
   // number of random bytes needed meet the required output length
-  const n = Math.ceil(length * Math.log2(alphabet.length) / Math.log2(256));
+  const size = Math.ceil(length * Math.log2(alphabet.length) / Math.log2(256));
   // encode random bytes in the output alphabet
-  const encoded = (0, baseEncode_1.baseEncode)(
-    [...(0, node_crypto_1.randomBytes)(n)],
-    alphabet,
-    { trim: false },
-  );
+  const encoded = (0, baseEncode_1.baseEncode)(rng(size), alphabet, {
+    trim: false,
+  });
   // trim characters off the front to meet required length
   return encoded.slice(encoded.length - length);
 }
--- src/randomString.ts
+++ src/randomString.ts
@@ -2,17 +2,23 @@ import { randomBytes } from "node:crypto";
 import { alphabets } from "./alphabets";
 import { baseEncode } from "./baseEncode";
 
+// default RNG, for node
+function nodeRng(size: number) {
+  return [...randomBytes(size)];
+}
+
 // generate a random string of the given length using the given alphabet
 export function randomString(
-  { length = 24, alphabet = alphabets.base62 }: {
+  { length = 24, alphabet = alphabets.base62, rng = nodeRng }: {
     length?: number;
     alphabet?: string;
+    rng?: (size: number) => Array<number>;
   } = {},
 ) {
   // number of random bytes needed meet the required output length
-  const n = Math.ceil(length * Math.log2(alphabet.length) / Math.log2(256));
+  const size = Math.ceil(length * Math.log2(alphabet.length) / Math.log2(256));
   // encode random bytes in the output alphabet
-  const encoded = baseEncode([...randomBytes(n)], alphabet, { trim: false });
+  const encoded = baseEncode(rng(size), alphabet, { trim: false });
   // trim characters off the front to meet required length
   return encoded.slice(encoded.length - length);
 }
